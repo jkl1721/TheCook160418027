f77d7180d682ee33c2546c4cd536f8ce84808842
Jackly Christanto Perdana <62692463+jkl1721@users.noreply.github.com>
2021-09-12 18:57:45 +0700

    First Init

---
+++ app/src/main/java/com/ubaya/thecook160418027/View/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ubaya.thecook160418027.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ubaya.thecook160418027.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
