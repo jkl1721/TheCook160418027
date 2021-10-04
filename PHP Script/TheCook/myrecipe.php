b790456786cd9bd603139ed70d8d7824c3ee13ce
Jackly Christanto Perdana <62692463+jkl1721@users.noreply.github.com>
2021-10-04 22:09:10 +0700

    UTS Done, Pushed PHP Scripts

---
+++ PHP Script/TheCook/myrecipe.php
@@ -0,0 +1,21 @@
+<?php
+$json = '
+[
+    {
+        "id": "4",
+        "name":"Burger Keju",
+        "bahan": "2 roti burger 
+        2 lembar Daging asap 
+        sesuai selera Selada 
+        sesuai selera Keju 
+        1 butir telor 
+        secukupnya Saos sambal 
+        secukupnya Mayonaise 
+        Margarin",
+        "langkah": "Pertama masak semua bahan isian, seperti roti, daging dan telor 
+        Setelah semua bahan isian sudah disiapkan dan dimasak, susun roti dan isian sesuai selera",
+        "imgUrl": "https://cms.sehatq.com/public/img/article_img/berbagai-alasan-kenapa-burger-tidak-sehat-untuk-anda-1616419559.jpg"
+    }
+    ]
+    ';
+echo $json;
---
+++ PHP Script/TheCook/recipes.php
@@ -0,0 +1,83 @@
+<?php
+$json = '
+[
+    {
+        "id": "1",
+        "name":"Sambal Udang Belimbing",
+        "bahan": "
+        1/4 kg udang 
+        10 buah belimbing wuluh 
+        10 buah cabe rawit mercon 
+        5 siung bawang merah 
+        1 batang sereh 
+        3 lembar daun jeruk 
+        3 SDM Minyak kelapa atau jika tidak ada bisa pakai minyak biasa",
+        "langkah": "
+        Kupas udang, garami sedikit dan goreng hingga matang 
+        Iris bawang, sereh, daun jeruk dan cabai 
+        Campurkan udang kedalam irisan bahan-bahan 
+        Uleg semua bahan termasuk udang 
+        Sesuaikan tingkat kehalusan dengan selera saja 
+        Tambahkan minyak goreng panas 
+        Tambahkan garam dan sedikit gula sesuai selera",
+        "imgUrl": "https://img-global.cpcdn.com/recipes/7b4c50372ec4b43a/680x482cq70/sambal-ganja-sambal-udang-belimbing-khas-aceh-foto-resep-utama.webp"
+    },
+   {
+        "id": "2",
+        "name":"Sayur Mentimun Kuah",
+        "bahan": "
+        3 btr telor ayam 
+        1 buah mentimun ukuran besar 
+        1 buah wortel ukuran sedang 
+        sesuai selera Cabe rawit merah ijo 
+        1 sdt kaldu bubuk totole/boleh pke kaldu ayam bubuk 
+        secukupnya Garam 
+        1 sdt Gula",
+        "langkah": "
+        Cuci dan kupas mentimun belah dua buang bijinya
+        potong2 demikian juga dgn wortel potong serong untuk cabe bisa diutuhkan atau diiris serong
+        Siapkan wajan dan tuang minyak goreng
+        ceplok telor satu persatu
+        untuk kematangan sesuai dgn selera
+        matang angkat sisihkan
+        Rebus air kurleb 400-500 ml sampai mendidih
+        masukan potongan wortel dan mentimun
+        tambahkan cabe rawit atau boleh diganti paprika bila tdk suka pedas
+        masak jgn lama2 biar tekstur mentimunnya crunchy tambahkan kaldu totole
+        garam dan gula,cicipi rasa,terahir masukan telor ceplok diatasnya
+        selesai siap dihidangkan",
+        "imgUrl": "https://img-global.cpcdn.com/recipes/b328d3723a576b12/680x482cq70/sayur-mentimun-kuah-telor-ceplok-foto-resep-utama.webp"
+    },
+    {
+        "id": "3",
+        "name":"Burger Dengan Roti",
+        "bahan": "2 roti burger 
+        2 lembar Daging asap 
+        sesuai selera Selada 
+        sesuai selera Keju 
+        1 butir telor 
+        secukupnya Saos sambal 
+        secukupnya Mayonaise 
+        Margarin",
+        "langkah": "Pertama masak semua bahan isian, seperti roti, daging dan telor 
+        Setelah semua bahan isian sudah disiapkan dan dimasak, susun roti dan isian sesuai selera",
+        "imgUrl": "https://img-global.cpcdn.com/recipes/0fc095096677b70a/680x482cq70/burger-homemade-praktis-dan-enak-foto-resep-utama.webp"
+    },
+    {
+        "id": "4",
+        "name":"Burger Keju",
+        "bahan": "2 roti burger 
+        2 lembar Daging asap 
+        sesuai selera Selada 
+        sesuai selera Keju 
+        1 butir telor 
+        secukupnya Saos sambal 
+        secukupnya Mayonaise 
+        Margarin",
+        "langkah": "Pertama masak semua bahan isian, seperti roti, daging dan telor 
+        Setelah semua bahan isian sudah disiapkan dan dimasak, susun roti dan isian sesuai selera",
+        "imgUrl": "https://cms.sehatq.com/public/img/article_img/berbagai-alasan-kenapa-burger-tidak-sehat-untuk-anda-1616419559.jpg"
+    }
+    ]
+    ';
+    echo $json;
