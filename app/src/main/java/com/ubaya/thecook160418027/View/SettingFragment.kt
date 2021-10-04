a94e658812a791b29e445fc447b21d6ed39c8ac7
Jackly Christanto Perdana <62692463+jkl1721@users.noreply.github.com>
2021-10-04 21:00:32 +0700

    UTS Done

--- app/src/main/java/com/ubaya/thecook160418027/View/SettingFragment.kt
+++ app/src/main/java/com/ubaya/thecook160418027/View/SettingFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.snackbar.Snackbar
 import com.ubaya.thecook160418027.R
+import kotlinx.android.synthetic.main.fragment_add_resep.*
+import kotlinx.android.synthetic.main.fragment_setting.*
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+
 
 class SettingFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +20,20 @@ class SettingFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            switchDark.isChecked = true
+        }else{
+            switchDark.isChecked = false
+        }
+        switchDark.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 }
